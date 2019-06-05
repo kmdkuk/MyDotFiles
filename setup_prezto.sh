8a2a498e631ce71189a351f784835037852f2fa1
kouki <kouworld0123@gmail.com>
2019-06-06 03:45:14 +0900

    :tada: setup用のshellscriptを作成

--- setup_prezto.sh
+++ setup_prezto.sh
@@ -6,3 +6,13 @@ setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+
+sudo apt-get install fonts-powerline
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
