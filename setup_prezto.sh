da29221839bcb80f4dbc1ad1856a603ea8b0791b
kouki <kouworld0123@gmail.com>
2019-09-24 04:01:13 -0700

    :+1: using nerd font

--- setup_prezto.sh
+++ setup_prezto.sh
@@ -7,12 +7,8 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
-sudo apt-get install fonts-powerline
-# clone
-git clone https://github.com/powerline/fonts.git --depth=1
-# install
-cd fonts
+git clone --branch=master --depth 1 https://github.com/ryanoasis/nerd-fonts.git
+cd nerd-fonts
 ./install.sh
-# clean-up a bit
 cd ..
-rm -rf fonts
+rm -rf nerd-fonts
