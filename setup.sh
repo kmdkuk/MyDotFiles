3396bd78e3d6d8caf79955eb84c62efa51a8aaed
kouki <kouworld0123@gmail.com>
2019-09-25 23:36:09 +0900

    :+1: adding gdb settings

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .Brewfile .github.zshrc .vimrc .tmux.conf .commit_template .gitconfig .gitignore_global .latexmkrc .zpreztorc .rubocop.yml .p10k.zsh)
+DOT_FILES=( .Brewfile .github.zshrc .vimrc .tmux.conf .commit_template .gitconfig .gitignore_global .latexmkrc .zpreztorc .rubocop.yml .p10k.zsh .gdbinit)
 
 for file in ${DOT_FILES[@]}
 do
