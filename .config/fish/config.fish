89e34ddab6d1ec9abb949e45a23467c78dc63547
kouki <kouworld0123@gmail.com>
2020-10-07 17:02:43 +0900

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,19 @@
 alias d="docker"
 alias dc="docker-compose"
-eval (hub alias -s fish)
+alias git='hub'
 alias vim='nvim'
 set EDITOR "vim"
 set -x PATH $HOME/.anyenv/bin $PATH
 set PATH $HOME/bin $PATH
+
+# theme-bobthefish
+set -g theme_nerd_fonts yes
+set -g heme_use_abbreviated_branch_name no
+set -g theme_newline_cursor yes
+set -g theme_display_git_master_branch yes
+set -g theme_display_git_ahead_verbose yes
+set -g theme_display_git_dirty_verbose yes
+set -g theme_display_git_stashed_verbose yes
+set -g theme_title_display_process yes
+set -g theme_color_scheme nord
 
