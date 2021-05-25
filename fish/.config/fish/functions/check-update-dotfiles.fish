fc003dd5ff501cbd791d944ae71fdc3af6a57d43
kouki <kouworld0123@gmail.com>
2021-05-26 00:16:48 +0900

    update

--- fish/.config/fish/functions/check-update-dotfiles.fish
+++ fish/.config/fish/functions/check-update-dotfiles.fish
@@ -1,16 +1,17 @@
 function check-update-dotfiles
-    set dotfiles_home "~/MyDotFiles"
-
-    if test -n "(git -C $dotfiles_home status --porcelain)"; or \
-            ! git -C $dotfiles_home diff --exit-code --stat --cached origin/main >/dev/null
+    set dotfiles_home "$HOME/MyDotFiles"
+    # 文字列空だったら１，０なら文字があった
+    git -C $dotfiles_home status --porcelain | string length >/dev/null
+    set s $status
+    git -C $dotfiles_home diff --exit-code --stat --cached origin/master | string length >/dev/null
+    set d $status
+    if test "$s" = "0"; or test "$d" = "0"
         echo -e "\e[36m=== DOTFILES IS DIRTY ===\e[m"
         echo -e "\e[33mThe dotfiles have been changed.\e[m"
         echo -e "\e[33mPlease update them with the following command.\e[m"
         echo "  cd $dotfiles_home"
         echo "  git add -A"
         echo "  git commit"
-        echo "  git push origin master"
-        echo -e "\e[33mor\e[m"
         echo "  git push origin master"
         echo -e "\e[36m=========================\e[m"
     end
