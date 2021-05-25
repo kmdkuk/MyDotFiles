0de2fa9fdc88707215e5eb3b77df24b319d570e5
kouki <kouworld0123@gmail.com>
2021-05-25 23:48:48 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,13 +1,49 @@
 #!/usr/bin/env bash
 
+echo "ファイルのシンボリックリンクを準備"
+echo "汎用ファイルを配置"
+# fish
+echo "fish"
 mkdir -p $HOME/.config/fish/functions
-ln -s $HOME/MyDotFiles/.config/fish/config.fish $HOME/.config/fish/config.fish
-ln -s $HOME/MyDotFiles/.config/fish/functions/ghq-cd.fish $HOME/.config/fish/functions/ghq-cd.fish
+ln -sf $HOME/MyDotFiles/.config/fish/config.fish $HOME/.config/fish/config.fish
+ln -sf $HOME/MyDotFiles/.config/fish/functions/ghq-cd.fish $HOME/.config/fish/functions/ghq-cd.fish
 
-DOT_FILES=(Brewfile .vimrc .tmux.conf .commit_template .gitconfig .gitignore_global .tmux)
+# vim
+echo "vim"
+ln -sf $HOME/MyDotFiles/vim/.vimrc $HOME/.vimrc
 
-for file in ${DOT_FILES[@]}
-do
-    ln -s $HOME/MyDotFiles/$file $HOME/$file
-done
+# git
+echo "git"
+mkdir -p $HOME/.config/git
+ln -sf $HOME/MyDotFiles/git/.gitconfig $HOME/.gitconfig
+ln -sf $HOME/MyDotFiles/git/.config/git/template $HOME/.config/git/template
+ln -sf $HOME/MyDotFiles/git/.config/git/ignore $HOME/.config/git/ignore
+
+# vim
+echo "vim"
+mkdir -p $HOME/.tmux
+ln -sf $HOME/MyDotFiles/tmux/.tmux/iceberg.tmux.conf $HOME/.tmux/iceberg.tmux.conf
+ln -sf $HOME/MyDotFiles/tmux/.tmux.conf $HOME/.tmux.conf
+
+# OSごとの分岐
+if [ "$(uname)" == 'Darwin' ]; then
+    # Mac
+    echo "Mac用のファイルを配置"
+    # brew
+    ln -sf $HOME/MyDotFiles/Brewfile $HOME/Brewfile
+    if [ -z "$(command -v brew)" ]; then
+        echo "--- Install Homebrew is Start! ---"
+
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        brew bundle
+
+        echo "--- Install Homebrew is Done!  ---"
+    fi
+    # tmux
+    ln -sf $HOME/MyDotFiles/tmux/.tmux/osx.tmux.conf $HOME/.tmux/local.tmux.conf
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    # Linux
+    echo "Linux用のファイルを配置"
+    ln -sf $HOME/MyDotFiles/tmux/.tmux/linux.tmux.conf $HOME/.tmux/local.tmux.conf
+fi
 
