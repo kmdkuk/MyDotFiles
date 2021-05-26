f457ec67e951ed7567ac5d803cffb8331aba1af3
kouki <kouworld0123@gmail.com>
2021-05-26 09:39:40 +0900

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
 alias d="docker"
 alias dc="docker-compose"
 alias git='hub'
-set -x EDITOR "vim"
+set -x EDITOR vim
 set -x PATH $HOME/bin $PATH
 
 # theme-bobthefish
@@ -15,7 +15,19 @@ set -g theme_display_git_stashed_verbose yes
 set -g theme_title_display_process yes
 set -g theme_color_scheme nord
 
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-source ~/.asdf/asdf.fish
+set -g fish_user_paths /usr/local/sbin $fish_user_paths
+
+if [ (uname) = Darwin ]
+    # OSX
+    echo "Welcom OSX"
+    source /usr/local/opt/asdf/asdf.fish
+else if [ (expr substr (uname -s) 1 5) == Linux ]
+    # Linux
+    echo "Welcom Linux"
+    source ~/.asdf/asdf.fish
+end
+
+# Hook direnv into your shell.
+eval (asdf exec direnv hook fish)
 
 check-update-dotfiles
