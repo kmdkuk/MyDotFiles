5a9dc8e177026e30442e5abbd8fdda1887138a2d
kouki <kouworld0123@gmail.com>
2021-05-31 10:19:47 +0900

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,11 +8,17 @@ set -x PATH $HOME/bin $PATH
 set -g theme_nerd_fonts yes
 set -g heme_use_abbreviated_branch_name no
 set -g theme_newline_cursor yes
+
 set -g theme_display_git_master_branch yes
 set -g theme_display_git_ahead_verbose yes
 set -g theme_display_git_dirty_verbose yes
 set -g theme_display_git_stashed_verbose yes
+
+set -g theme_display_k8s_context yes
+set -g theme_display_k8s_namespace yes
+
 set -g theme_title_display_process yes
+
 set -g theme_color_scheme nord
 
 set -g fish_user_paths /usr/local/sbin $fish_user_paths
