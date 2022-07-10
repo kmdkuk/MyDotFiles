b4928319801008eb1fa740c9533f8777129d9796
kouki <kouworld0123@gmail.com>
2022-07-10 12:29:43 +0900

    update
    Signed-off-by: kouki <kouworld0123@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+set -eux
+
 dotfiles_home=${HOME}/MyDotFiles
 if [ ! -d ${dotfiles_home} ]; then
     git clone https://github.com/kmdkuk/MyDotFiles.git ${dotfiles_home}
 fi
 
-echo "ファイルのシンボリックリンクを準備"
-echo "fish"
+: "prepare shimlink"
+: "fish"
 mkdir -p ${HOME}/.config/fish/functions
 ln -sf ${dotfiles_home}/fish/.config/fish/config.fish ${HOME}/.config/fish/config.fish
 mkdir -p ${HOME}/.config/fish/completions;
@@ -14,47 +16,47 @@ ln -sf ${HOME}/.asdf/completions/asdf.fish ${HOME}/.config/fish/completions
 ln -sf ${dotfiles_home}/fish/.config/fish/functions/ghq-cd.fish ${HOME}/.config/fish/functions/ghq-cd.fish
 ln -sf ${dotfiles_home}/fish/.config/fish/functions/check-update-dotfiles.fish ${HOME}/.config/fish/functions/check-update-dotfiles.fish
 
-echo "bash/zsh"
+: "bash/zsh"
 ln -sf ${dotfiles_home}/.bashrc ${HOME}/.bashrc
 ln -sf ${dotfiles_home}/.zshrc ${HOME}/.zshrc
 
-echo "starship"
+: "starship"
 ln -sf ${dotfiles_home}/starship.toml ${HOME}/.config/starship.toml
 
-echo "vim"
+: "vim"
 ln -sf ${dotfiles_home}/vim/.vimrc ${HOME}/.vimrc
 
-echo "git"
+: "git"
 mkdir -p ${HOME}/.config/git
 ln -sf ${dotfiles_home}/git/.config/git/config ${HOME}/.config/git/config
 ln -sf ${dotfiles_home}/git/.config/git/template ${HOME}/.config/git/template
 ln -sf ${dotfiles_home}/git/.config/git/ignore ${HOME}/.config/git/ignore
 ln -sf ${dotfiles_home}/git/.config/git/work.config ${HOME}/.config/git/work.config
 
-echo "tmux"
+: "tmux"
 mkdir -p ${HOME}/.tmux
 ln -sf ${dotfiles_home}/tmux/.tmux/iceberg.tmux.conf ${HOME}/.tmux/iceberg.tmux.conf
 ln -sf ${dotfiles_home}/tmux/.tmux.conf ${HOME}/.tmux.conf
 
-echo "asdf"
+: "asdf"
 ln -sf ${dotfiles_home}/asdf/.asdfrc ${HOME}/.asdfrc
 ln -sf ${dotfiles_home}/asdf/.tool-versions ${HOME}/.tool-versions
 
 # OSごとの分岐
 if [ "$(uname)" == 'Darwin' ]; then
-    echo "Mac"
+    : "Mac"
     # brew
     ln -sf ${dotfiles_home}/Brewfile ${HOME}/Brewfile
     if [ -z "$(command -v brew)" ]; then
-        echo "--- Install Homebrew is Start! ---"
+        : "--- Install Homebrew is Start! ---"
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        echo "--- Install Homebrew is Done!  ---"
+        : "--- Install Homebrew is Done!  ---"
     fi
     brew bundle check | grep -q "brew bundle can't satisfy your Brewfile's dependencies."
     if [ $? = 0 ]; then
-        echo "Start bundle install"
+        : "Start bundle install"
         brew bundle
-        echo "Done bundle install"
+        : "Done bundle install"
     fi
     # tmux
     ln -sf ${dotfiles_home}/tmux/.tmux/osx.tmux.conf ${HOME}/.tmux/local.tmux.conf
@@ -65,17 +67,17 @@ if [ "$(uname)" == 'Darwin' ]; then
     defaults write com.apple.finder CreateDesktop -boolean false
     killAll Finder
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-    echo "Linux"
+    : "Linux"
     ln -sf ${dotfiles_home}/tmux/.tmux/linux.tmux.conf ${HOME}/.tmux/local.tmux.conf
     # git
     ln -sf ${dotfiles_home}/git/.config/git/linux.config ${HOME}/.config/git/local.config
 fi
 
-echo "bin"
+: "bin"
 mkdir -p ${HOME}/bin
 bins="$(ls ${dotfiles_home}/bin)"
 for b in ${bins[@]}; do
     ln -sf ${dotfiles_home}/bin/$b ${HOME}/bin/$b
 done
 
-echo "install tools (TODO)"
+: "install tools (TODO)"
