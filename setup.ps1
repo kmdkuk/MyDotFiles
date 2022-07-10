6fa023a7b6730dfa4c3f0ee87b79d6693f2ba0fe
actions <actions@users.noreply.github.com>
2022-07-10 06:49:48 +0000

    update

--- setup.ps1
+++ setup.ps1
@@ -1,28 +1,52 @@
+$ErrorActionPreference = "Stop"
+
+if(!(where.exe choco)){
+    Write-Output "install choco"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+if(!(where.exe git)) {
+    Write-Output "install git"
+    choco install git
+}
+
+$DOTFILES_HOME="$HOME\MyDotFiles"
+if(!(Test-Path -Path $DOTFILES_HOME )) {
+    git clone https://github.com/kmdkuk/MyDotFiles.git $DOTFILES_HOME
+}
+
+Write-Output "prepare symlink"
 # starship
 Write-Output "starship"
-New-Item -Type SymbolicLink $HOME\.config\starship.toml -Value $HOME\MyDotFiles\starship.toml
+New-Item -Force -Type SymbolicLink $HOME\.config\starship.toml -Value $DOTFILES_HOME\starship.toml
 
 # vim
 Write-Output "vim"
-New-Item -Type SymbolicLink $HOME\.vimrc -Value $HOME\MyDotFiles\vim\.vimrc
+New-Item -Force -Type SymbolicLink $HOME\.vimrc -Value $DOTFILES_HOME\vim\.vimrc
 
 # git
 Write-Output "git"
-mkdir -p $HOME\.config\git
-New-Item -Type SymbolicLink $HOME\.config\git\config -Value $HOME\MyDotFiles\git\.config\git\config
-New-Item -Type SymbolicLink C:\ProgramData\Git\config -Value $HOME\MyDotFiles\git\.config\git\config
-New-Item -Type SymbolicLink $HOME\.config\git\template -Value $HOME\MyDotFiles\git\.config\git\template
-New-Item -Type SymbolicLink $HOME\.config\git\ignore -Value $HOME\MyDotFiles\git\.config\git\ignore
-New-Item -Type SymbolicLink $HOME\.config\git\local.config -Value $HOME\MyDotFiles\git\.config\git\win.config
-New-Item -Type SymbolicLink $HOME\.config\git\work.config -Value $HOME\MyDotFiles\git\.config\git\work.config
+if(!(Test-Path -Path $HOME\.config\git)){
+    mkdir -p $HOME\.config\git
+}
+New-Item -Force -Type SymbolicLink $HOME\.config\git\config -Value $DOTFILES_HOME\git\.config\git\config
+New-Item -Force -Type SymbolicLink C:\ProgramData\Git\config -Value $DOTFILES_HOME\git\.config\git\config
+New-Item -Force -Type SymbolicLink $HOME\.config\git\template -Value $DOTFILES_HOME\git\.config\git\template
+New-Item -Force -Type SymbolicLink $HOME\.config\git\ignore -Value $DOTFILES_HOME\git\.config\git\ignore
+New-Item -Force -Type SymbolicLink $HOME\.config\git\local.config -Value $DOTFILES_HOME\git\.config\git\win.config
+New-Item -Force -Type SymbolicLink $HOME\.config\git\work.config -Value $DOTFILES_HOME\git\.config\git\work.config
 
 # zsh/bash
 Write-Output "zsh/bash"
-New-Item -Type SymbolicLink $HOME\.bashrc -Value $HOME\MyDotFiles\.bashrc
-New-Item -Type SymbolicLink $HOME\.zshrc -Value $HOME\MyDotFiles\.zshrc
+New-Item -Force -Type SymbolicLink $HOME\.bashrc -Value $DOTFILES_HOME\.bashrc
+New-Item -Force -Type SymbolicLink $HOME\.zshrc -Value $DOTFILES_HOME\.zshrc
 
 # Powershell profile
 Write-Output "Powershell profile"
-New-Item -Type SymbolicLink $HOME\Documents\WindowsPowerShell\Microsoft.VSCode_profile.ps1 -Value $HOME\MyDotFiles\windows\Microsoft.PowerShell_profile.ps1
-New-Item -Type SymbolicLink $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Value $HOME\MyDotFiles\windows\Microsoft.PowerShell_profile.ps1
-New-Item -Type SymbolicLink $HOME\Documents\WindowsPowerShell\Microsoft.Powershell_profile.ps1 -Value $HOME\MyDotFiles\windows\Microsoft.PowerShell_profile.ps1
+New-Item -Force -Type SymbolicLink $HOME\Documents\WindowsPowerShell\Microsoft.VSCode_profile.ps1 -Value $DOTFILES_HOME\windows\Microsoft.PowerShell_profile.ps1
+New-Item -Force -Type SymbolicLink $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Value $DOTFILES_HOME\windows\Microsoft.PowerShell_profile.ps1
+New-Item -Force -Type SymbolicLink $HOME\Documents\WindowsPowerShell\Microsoft.Powershell_profile.ps1 -Value $DOTFILES_HOME\windows\Microsoft.PowerShell_profile.ps1
+
+Write-Output "install tools"
+.\rm-version.ps1
+choco install -y .\choco-rm-version.config
