61bf064d6ec0fbf07d79275241eb9fb195b2e405
kouki <kouworld0123@gmail.com>
2022-07-10 13:12:14 +0900

    update
    Signed-off-by: kouki <kouworld0123@gmail.com>

--- setup.ps1
+++ setup.ps1
@@ -13,6 +13,7 @@ New-Item -Type SymbolicLink $HOME\.config\git\config -Value $HOME\MyDotFiles\git
 New-Item -Type SymbolicLink C:\ProgramData\Git\config -Value $HOME\MyDotFiles\git\.config\git\config
 New-Item -Type SymbolicLink $HOME\.config\git\template -Value $HOME\MyDotFiles\git\.config\git\template
 New-Item -Type SymbolicLink $HOME\.config\git\ignore -Value $HOME\MyDotFiles\git\.config\git\ignore
+New-Item -Type SymbolicLink $HOME\.config\git\local.config -Value $HOME\MyDotFiles\git\.config\git\win.config
 New-Item -Type SymbolicLink $HOME\.config\git\work.config -Value $HOME\MyDotFiles\git\.config\git\work.config
 
 # zsh/bash
@@ -23,4 +24,5 @@ New-Item -Type SymbolicLink $HOME\.zshrc -Value $HOME\MyDotFiles\.zshrc
 # Powershell profile
 Write-Output "Powershell profile"
 New-Item -Type SymbolicLink $HOME\Documents\WindowsPowerShell\Microsoft.VSCode_profile.ps1 -Value $HOME\MyDotFiles\windows\Microsoft.PowerShell_profile.ps1
+New-Item -Type SymbolicLink $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Value $HOME\MyDotFiles\windows\Microsoft.PowerShell_profile.ps1
 New-Item -Type SymbolicLink $HOME\Documents\WindowsPowerShell\Microsoft.Powershell_profile.ps1 -Value $HOME\MyDotFiles\windows\Microsoft.PowerShell_profile.ps1
