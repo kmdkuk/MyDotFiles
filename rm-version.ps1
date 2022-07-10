62cedbed5ba0337f94ac461462f2fb59e7edad9d
kouki <kouworld0123@gmail.com>
2022-07-10 15:49:16 +0900

    Add cloning steps, if not found dotfiles
    Signed-off-by: kouki <kouworld0123@gmail.com>

--- rm-version.ps1
+++ rm-version.ps1
@@ -3,4 +3,4 @@ $xmldoc = [xml](Get-Content .\$file)
 for($i = 0; $i -lt $xmldoc.packages.package.Count; $i++) {
     $xmldoc.packages.package[$i].RemoveAttribute("version")
 }
-$xmldoc.Save(".\$file")
+$xmldoc.Save(".\choco-rm-version.config")
